f823f2796b2a8587c1355593fe43bfb67e309bd5
ggeorgiu <ggeorgiu@uw.co.uk>
2024-04-05 00:22:56 +0300

    Tidy up setup script

--- setup.sh
+++ setup.sh
@@ -5,14 +5,15 @@ list_dir_cmd=$(find . -maxdepth 1 -type d -not -path "./.*" -not -path "." | sed
 
 echo "Starting configuration..."
 for dir in $list_dir_cmd; do 
-    echo "$dir -> y/n?"
+    echo -n "$dir -> y/n? "
     read -r yn
     case $yn in 
         [yY])
-            stow "$dir"
+            stow -v "$dir"
+            echo "Configured $dir"
             ;;
         *)
-            echo "Skipping $dir..."
+            echo "Skipped $dir"
             ;;
     esac
 done
