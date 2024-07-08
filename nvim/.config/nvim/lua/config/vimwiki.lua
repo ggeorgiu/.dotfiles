b5d3690b409ef48c7f73fff84236cfdf19181f76
ggeorgiu <ggeorgiu@uw.co.uk>
2024-07-08 10:39:04 +0300

    Update vimwiki list

--- nvim/.config/nvim/lua/config/vimwiki.lua
+++ nvim/.config/nvim/lua/config/vimwiki.lua
@@ -1,6 +1,5 @@
 -- Default settings
 vim.o.compatible = false
-vim.cmd("filetype plugin on")
 
 -- Wiki list
 vim.g.vimwiki_global_ext = 0
@@ -8,13 +7,31 @@ vim.g.vimwiki_list = {
 	{
 		path = "~/notes/work/",
 		syntax = "markdown",
-		ext = ".md",
-		index = "work",
+		ext = "md",
+		index = "to-do",
 	},
 	{
-		path = "~/notes/personal/",
+		path = "~/notes/personal/daily/",
 		syntax = "markdown",
-		ext = ".md",
+		ext = "md",
 		index = "personal",
+	},
+	{
+		path = "~/notes/personal/learn/",
+		syntax = "markdown",
+		ext = "md",
+		index = "learn",
+	},
+	{
+		path = "~/notes/personal/gym/",
+		syntax = "markdown",
+		ext = "md",
+		index = "gym",
+	},
+	{
+		path = "~/notes/personal/finance/",
+		syntax = "markdown",
+		ext = "md",
+		index = "finance",
 	},
 }
